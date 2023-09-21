9a3980676716bd610473ca1264e771e039bf47b4
Hau281073 <141088563+Hau281073@users.noreply.github.com>
2023-09-21 21:53:04 +0700

    upload lan 1

---
+++ filer.php
@@ -0,0 +1,20 @@
+<div class="bg-none">
+    <div class="filer bg-white ps-5 pe-5">
+        <div class="filer-dropdown container-fuild d-flex justify-content-between align-items-center class-pointer pt-4">
+            <div class="nav-item color-primary d-flex align-items-center">
+                <a href="#" class="nav-link">Filter</a>
+            </div>
+            <div class="rounded-circle mm-active text-center icon-chevron"><i class="bi bi-chevron-up text-white"></i>
+            </div>
+        </div>
+        <form class="form-group mt-3 d-flex justify-content-between">
+            <input type="text" id="Email" placeholder="Email" class="form-control">
+            <input type="text" id="Mobile" placeholder="Mobile" class="form-control">
+            <select class="form-select">
+                <option>Select Group</option>
+            </select>
+            <button class="btn btn-filter"><i class="bi bi-search"></i> <span>Filter</span></button>
+            <button class="btn btn-clear">Clear</button>
+        </form>
+    </div>
+</div>
